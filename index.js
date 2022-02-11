277eb8c32bb534ec6e227e574bce23741ef474e1
Allen Mathews <allenjoemathews@gmail.com>
2022-02-11 17:33:08 +1100

    redone assignment

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 function shout(string) {
-    return string.toUpperCase();
+    return string.toUpperCase()
 }
+
+shout("hello")
 
 function whisper(string) {
-    return string.toLowerCase();
+    return string.toLowerCase()
 }
+whisper("HELLO")
 
 function logShout(string) {
-    console.log(string.toUpperCase());
-};
+    console.log(string.toUpperCase())
+}
+
+shoutString("hello")
 
 function logWhisper(string) {
-    console.log(string.toLowerCase());
-};
+    console.log(string.toLowerCase())
+}
+
+logWhisper("HELLO")
 
 function sayHiToGrandma(string) {
     if (string.toLowerCase() === string) {
         return "I can't hear you!"
-    }
-    if (string.toUpperCase() === string){
+    } else if (string.toUpperCase() === string) {
         return "YES INDEED!"
-    }
-    if (string === "I love you, Grandma."){
-        return "I love you, too."
-    }
+    } else(string === "I love you, Grandma.")
+    return "I love you, too."
 }
